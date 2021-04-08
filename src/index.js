9fbc5f57998756c4e1815992e61ba4012625900a
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-04-07 21:58:07 -0400

    added api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("Hello World");
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
+let apiKey = "1112fe88e93908caf3cc29e57ec1349e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
