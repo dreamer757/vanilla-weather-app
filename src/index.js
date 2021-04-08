d0aa05b5ac3bcbb3c08e93234f5f62986a41e2d8
dreamer757 <30564161+dreamer757@users.noreply.github.com>
2021-04-07 22:20:34 -0400

    added functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "1112fe88e93908caf3cc29e57ec1349e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Virginia Beach&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
